fd5f823cd09ad6a977b8861e8b28f46029fa3acc
p12miel <anton.rouh@epita.fr>
2022-09-20 15:12:27 +0200

    ajout switch et fibo au Launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,12 +5,23 @@ public class Launcher {
         System.out.println("Bienvenue sur le Launcher !");
         Scanner sc = new Scanner(System.in);
         while(true) {
-            String inp = sc.nextLine();
-            if ("quit".equals(inp)) {
-                break;
-            } else {
-                System.out.println("Unknown command");
+            switch(sc.nextLine()){
+                case "quit":
+                    return;
+                case "fibo":
+                    System.out.println("Entrez l'index du nombre que vous chercher dans la séquence de fibo ");
+                    int inp2 = Integer.parseInt(sc.nextLine());
+                    System.out.println("le résultat est "+fibo(inp2));
+                    break;
+                default:
+                    System.out.println("Unknown command");
+                    break;
             }
         }
+    }
+    public static int fibo(int n){
+        if (n <= 1)
+            return n;
+        return fibo(n-1) + fibo(n-2);
     }
 }
