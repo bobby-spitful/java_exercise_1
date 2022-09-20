2075f227e14577ad21f1d123439b839c488a6818
p12miel <anton.rouh@epita.fr>
2022-09-20 14:48:33 +0200

    boucle Launcher terminée

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,9 +4,13 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Bienvenue sur le Launcher !");
         Scanner sc = new Scanner(System.in);
-        String inp = sc.nextLine();
-        if (inp != "quit"){
+        while(true) {
+            String inp = sc.nextLine();
+            if ("quit".equals(inp)) {
+                break;
+            } else {
                 System.out.println("Unknown command");
+            }
         }
     }
 }
